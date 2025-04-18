d52c3b8cb3dbaf9d568a4f3dc5bfff8c634af9d6
NKO_Gallardo <nkogalado@gmail.com>
2025-04-18 13:26:15 +0200

    Update me.js
    send email to me

--- me.js
+++ me.js
@@ -3,11 +3,22 @@
     mobileNav.classList.toggle("show");
   }
 
-  emailjs.sendForm('service_5mw71gy', 'template_88pph0e', '#myForm').then(
-    (response) => {
-      console.log('SUCCESS!', response.status, response.text);
-    },
-    (error) => {
-      console.log('FAILED...', error);
-    },
-  );
+  function sendEmail () {
+    const templateParams = {
+      name: document.getElementById("#name").value,
+      email: document.getElementById("#Email").value,
+      message: document.getElementById("#message").value,
+      phone: document.getElementById("#phone").value,
+      birthdate: document.getElementById("#Birthdate").value,
+  }
+
+  emailjs
+  .send("service_5mw71gy", "template_88pph0e", templateParams,)
+  .then(() => {
+    alert("Email sent successfully!");
+  })
+  .catch((error) => {
+    console.error("Error sending email:", error);
+    alert("Failed to send email. Please try again later.");
+  });
+}
