d15be389ba274b5c606e73de98881df9ea9d1ba6
NKO_Gallardo <nkogalado@gmail.com>
2025-04-18 12:47:57 +0200

    Update me.js
    form code

--- me.js
+++ me.js
@@ -1,4 +1,13 @@
-/* Nav menu ❔ which not working */
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+  function toggleMobileMenu(icon) {
+    const mobileNav = icon.querySelector(".mobile");
+    mobileNav.classList.toggle("show");
+  }
+
+  emailjs.sendForm('service_5mw71gy', 'template_88pph0e', '#myForm').then(
+    (response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    },
+    (error) => {
+      console.log('FAILED...', error);
+    },
+  );
