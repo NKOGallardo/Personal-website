0f3b2a4399eb1bd934a63d7c86f7a26a935de376
NKOGallardo <nkogalado@gmail.com>
2026-08-28 15:58:33 +0200

    added for mobile

--- script/snake.js
+++ script/snake.js
@@ -58,7 +58,7 @@
     if (snake.cells.length > snake.maxCells) snake.cells.pop();
 
     // draw apple
-    context.fillStyle = 'red';
+    context.fillStyle = 'red';-
     context.fillRect(apple.x, apple.y, grid-1, grid-1);
 
     // draw snake
@@ -102,5 +102,74 @@
     apple.x = getRandomInt(0, cols) * grid;
     apple.y = getRandomInt(0, rows) * grid;
   })();
+
+  // Mobile controls
+  function changeDirection(direction) {
+  if (direction === "up" && snake.dy === 0) {
+    snake.dx = 0;
+    snake.dy = -grid;
+  }
+
+  else if (direction === "down" && snake.dy === 0) {
+    snake.dx = 0;
+    snake.dy = grid;
+  }
+
+  else if (direction === "left" && snake.dx === 0) {
+    snake.dx = -grid;
+    snake.dy = 0;
+  }
+
+  else if (direction === "right" && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
+  }
+}
+
+document.querySelectorAll("#mobileControls button").forEach(button => {
+  button.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+
+    const direction = this.dataset.dir;
+    changeDirection(direction);
+  });
+});
+
+// mobile friendly screen
+  function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+let touchStartX = 0;
+let touchStartY = 0;
+
+canvas.addEventListener("touchstart", function(e) {
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+
+// Touch controls 
+canvas.addEventListener("touchend", function(e) {
+  const touchEndX = e.changedTouches[0].clientX;
+  const touchEndY = e.changedTouches[0].clientY;
+
+  const dx = touchEndX - touchStartX;
+  const dy = touchEndY - touchStartY;
+
+  if (Math.abs(dx) > Math.abs(dy)) {
+    if (dx > 30) {
+      changeDirection("right");
+    } else if (dx < -30) {
+      changeDirection("left");
+    }
+  } else {
+    if (dy > 30) {
+      changeDirection("down");
+    } else if (dy < -30) {
+      changeDirection("up");
+    }
+  }
+});
 
   requestAnimationFrame(loop);
